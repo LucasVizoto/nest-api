8214ed246a999aca8408138fc81747cae98524ab
Lucas Vizoto <lucasvizoto364@gmail.com>
2026-01-03 22:47:57 -0300

    feat: implementing create questions controller and a decorator to get only the user sub from request

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,24 +1,20 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  Post,
-  UseGuards,
-  UsePipes,
-} from "@nestjs/common";
+import { Body, Controller, HttpCode, Post, UseGuards } from "@nestjs/common";
 
 import { PrismaService } from "src/prisma/prisma.service";
-import { z } from "zod";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { CurrentUser } from "src/auth/current-user-decorator";
+import type { UserPayload } from "src/auth/jwt.strategy";
+import z from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 
-const createQuestionBodySchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
+const createQuestiontBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
 });
 
-type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>;
+const bodyValidationPipe = new ZodValidationPipe(createQuestiontBodySchema);
+
+type CreateQuestiontBodySchema = z.infer<typeof createQuestiontBodySchema>;
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
@@ -27,6 +23,29 @@ export class CreateQuestionController {
 
   @Post()
   @HttpCode(201)
-  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-  async handle(@Body() body: CreateQuestionBody) {}
+  async handle(
+    @Body(bodyValidationPipe)
+    body: CreateQuestiontBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body;
+    const userId = user.sub;
+    const slug = this.convertToSlug(title);
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    });
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+  }
 }
