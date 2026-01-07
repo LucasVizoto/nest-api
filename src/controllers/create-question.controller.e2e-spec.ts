e7e9aa4f1619d9465f32fa47295f4ba79718f2a0
Lucas Vizoto <lucasvizoto364@gmail.com>
2026-01-06 22:34:42 -0300

    feat: implementing e2e controller tests and schema database logic

---
+++ src/controllers/create-question.controller.e2e-spec.ts
@@ -0,0 +1,57 @@
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { describe, expect, test } from "vitest";
+
+describe("Create account (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+    await app.init();
+  });
+
+  test("[POST] /questions", async () => {
+    await prisma.user.create({
+      data: {
+        name: "John Doe",
+        email: "johndoe@example.com",
+        password: await hash("123456", 8),
+      },
+    });
+
+    const authResponse = await request(app.getHttpServer())
+      .post("/sessions")
+      .send({
+        email: "johndoe@example.com",
+        password: "123456",
+      });
+
+    const response = await request(app.getHttpServer())
+      .post("/questions")
+      .set("Authorization", `Bearer ${authResponse}`)
+      .send({
+        title: "New Question",
+        content: "Question Content",
+      });
+
+    console.log(response.body);
+    expect(response.statusCode).toBe(201);
+
+    const questionOnDatabase = await prisma.question.findFirst({
+      where: {
+        title: "New Question",
+      },
+    });
+
+    expect(questionOnDatabase).toBeTruthy();
+  });
+});
