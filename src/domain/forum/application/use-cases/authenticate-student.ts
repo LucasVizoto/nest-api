6f7bb557bbd07be2e7483e767a17bb1f21c52e5e
Lucas Vizoto <lucasvizoto364@gmail.com>
2026-01-13 20:24:46 -0300

    feat: implementing create and authenticate users use-cases

---
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -0,0 +1,54 @@
+import { left, right, type Either } from "@/core/either.js";
+import { Injectable } from "@nestjs/common";
+import { StudentsRepository } from "../repositories/students-repository.js";
+import { HashComparer } from "../cryptography/hash-comparer.js";
+import { Encrypter } from "../cryptography/encrypter.js";
+import { WrongCredentialsError } from "./errors/wrong-credentials-error.js";
+
+interface AuthenticateStudentUseCaseRequest {
+  email: string;
+  password: string;
+}
+type AuthenticateStudentUseCaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string;
+  }
+>;
+
+@Injectable()
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email);
+
+    if (!student) {
+      return left(new WrongCredentialsError());
+    }
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      student.password,
+    );
+
+    if (!isPasswordValid) {
+      return left(new WrongCredentialsError());
+    }
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString(),
+    });
+
+    return right({
+      accessToken,
+    });
+  }
+}
