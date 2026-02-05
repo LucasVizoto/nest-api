964731c964e6f197741544cd61e2643bbbca02c3
Lucas Vizoto <lucasvizoto364@gmail.com>
2026-02-04 22:28:30 -0300

    feat: implementing tests to Domain Events and adding the domain call in the prisma repository

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -7,6 +7,7 @@ import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { PrismaQuestionDetailsMapper } from "../mappers/prisma-question-details-mapper";
 import { QuestionDetails } from "@/domain/forum/enterprise/entities/value-objects/question-details";
+import { DomainEvents } from "@/core/events/domain-events";
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionRepository {
@@ -92,6 +93,8 @@ export class PrismaQuestionsRepository implements QuestionRepository {
         question.attachments.getRemovedItems(),
       ),
     ]);
+
+    DomainEvents.dispatchEventsForAggregate(question.id);
   }
 
   async create(question: Question): Promise<void> {
@@ -103,6 +106,8 @@ export class PrismaQuestionsRepository implements QuestionRepository {
     await this.questionAttachmentsRepository.createMany(
       question.attachments.getItems(),
     );
+
+    DomainEvents.dispatchEventsForAggregate(question.id);
   }
 
   async delete(question: Question): Promise<void> {
